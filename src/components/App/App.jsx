573f8ecfcb117632055c133208a338e4a26231ec
Tatiana-Hela <malya9472@gmail.com>
2023-01-28 10:56:37 +0300

    s

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,3 @@
-import { Component } from 'react';
-import axios from 'axios';
+// import { Component } from 'react';
+// import axios from 'axios';
 // import css from '../App/App.module.css'
-
-class App extends Component {}
-export default App;
